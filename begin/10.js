4a9a40bd4f7b70046b412f7277d42dcb01059e8a
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-01-25 15:44:28 +0500

    reshil begin10

---
+++ begin/10.js
@@ -0,0 +1,7 @@
+const a = 25;
+const b = 15;
+const c = Math.pow(a, 2) + Math.pow(b, 2);
+const d = Math.pow(a, 2) - Math.pow(b, 2);
+const i = Math.pow(a, 2) * Math.pow(b, 2);
+const f = Math.pow(a, 2) / Math.pow(b, 2);
+console.log(c, d, i, f);
