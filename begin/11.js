d3113220bfcb9f11092721508acbce9528f0132a
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-01-25 15:56:20 +0500

    reshil begin11

---
+++ begin/11.js
@@ -0,0 +1,7 @@
+const a = 25;
+const b = 15;
+const c = Math.abs(a) + Math.abs(b);
+const d = Math.abs(a) - Math.abs(b);
+const i = Math.abs(a) * Math.abs(b);
+const f = Math.abs(a) / Math.abs(b);
+console.log(c, d, i, f);
