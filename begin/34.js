5b0e60d1413434e7d61b61f8efe288c7145a5172
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-01 16:35:45 +0500

    reshil begin34

---
+++ begin/34.js
@@ -0,0 +1,10 @@
+const x_kg = 4;
+const a_rubley = 8;
+const y_kg = 2;
+const b_rubley = 4;
+const x_rubley = a_rubley / x_kg;
+console.log(x_rubley);
+const y_rubley = b_rubley / y_kg;
+console.log(y_rubley);
+const shokolad = x_rubley - y_rubley;
+console.log(shokolad);
