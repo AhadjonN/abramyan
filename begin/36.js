38a50f555b6dcbdcbedef4ea28b9b8256acee393
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-01 17:29:52 +0500

    reshil begin36

---
+++ begin/36.js
@@ -0,0 +1,10 @@
+const V1_km_ch = 15;
+const V2_km_ch = 12;
+const S = 4;
+const T = 6;
+const speed = V1_km_ch + V2_km_ch;
+console.log(speed);
+const path = speed * T;
+const D = S - path;
+console.log(D);
+
