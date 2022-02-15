13ceff621ddc864cb8698180ad93ea929f3847d3
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-15 17:03:34 +0500

    reshil boolean33

--- 33.js
+++ 33.js
@@ -1,5 +1,5 @@
 const a = parseInt(prompt());
 const b = parseInt(prompt());
 const c = parseInt(prompt());
-const s = (a == b + c * c) || (b == a + c * c) || (c == b + a * a);
+const s = (a < b + c) || (b < a + c) || (c < b + a)
 console.log(s);
