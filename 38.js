5dc35bda5509a12ce027e28a193523f7aace1e5d
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-16 15:40:08 +0500

    reshil boolean38

---
+++ 38.js
@@ -0,0 +1,8 @@
+const x1 = parseInt(prompt());
+const y1 = parseInt(prompt());
+const x2 = parseInt(prompt());
+const y2 = parseInt(prompt());
+const C = (x < 9 && y < 9);
+const D = (x1 < 9 && y1 < 9);
+const V = (C && D && ((Math.abs(x - x1) > 0 && Math.abs(y - y1) > 0) || (Math.abs(x - x1) <= 0 && Math.abs(y - y1) <= 0)));
+console.log(V);
