b4becc026ff316ba5cc71600950756b37cf6d544
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-16 17:15:28 +0500

    reshil if3

---
+++ if/3.js
@@ -0,0 +1,12 @@
+let a = parseInt(prompt());
+if (a >= 1) {
+    a = a + 1;
+    console.log(a)
+} else if (a < 0) {
+    a = a - 2;
+    console.log(a);
+} else {
+    a = a + 10;
+    console.log(a)
+}
+
