658ff206645aaba2d7df6ff4518de85b343ac812
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-17 16:09:21 +0500

    reshil if10

---
+++ if/10.js
@@ -0,0 +1,9 @@
+const a = parseInt(prompt());
+const b = parseInt(prompt());
+if (a !== b) {
+    const s = a + b;
+    console.log(s);
+} else if (a == b) {
+    const g = a * 0 && b * 0;
+    console.log(g);
+}
