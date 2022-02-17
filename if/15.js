416ca5ebe8a1b62552df09eb912126017bf3b778
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-17 17:32:14 +0500

    reshil if15

--- if/15.js
+++ if/15.js
@@ -3,12 +3,15 @@ let b = parseInt(prompt());
 let c = parseInt(prompt());
 
 if ((a > b && b > c) || (a < b && a > c)) {
-    console.log(b + a);
+    console.log(a);
+    console.log(b);
 
 }
 else if ((b < a && a < c) || (b < c && a > c)) {
-    console.log(a + c);
+    console.log(a);
+    console.log(c);
 }
 else {
-    console.log(c + b);
+    console.log(c);
+    console.log(b);
 }
