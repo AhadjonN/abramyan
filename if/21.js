de7b9ae565dc963749cf927769b4dcd548cb0530
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-18 16:15:36 +0500

    reshil if21

---
+++ if/21.js
@@ -0,0 +1,11 @@
+let x = parseInt(prompt());
+let y = parseInt(prompt());
+if (x == 0 && y == 0) {
+    console.log("0");
+} else if (x != 0) {
+    console.log("1");
+} else if (y != 0) {
+    console.log("2");
+} else {
+    console.log("3");
+}
