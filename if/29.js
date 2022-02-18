153c20a8d503907ef60a822acd274d0f37265e0f
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-18 17:29:29 +0500

    reshil if29

---
+++ if/29.js
@@ -0,0 +1,12 @@
+const g = parseInt(prompt());
+
+if (g === 0) {
+    console.log("Нулевое число.");
+}
+else if (g > 0) {
+    console.log("Положительное и ");
+} if (g % 2 === 0) {
+    console.log("Четное");
+} else {
+    console.log("Нечетное")
+}
