412534a36f2acb9a8699c876993acb0aa8596ccd
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-21 16:51:56 +0500

    reshil case4

---
+++ case/4.js
@@ -0,0 +1,66 @@
+let fasli_sol = parseInt(prompt("vividete ot 1 do 12:"));
+switch (fasli_sol) {
+    case 1:
+        console.log("yanvar");
+        console.log("zima");
+        console.log("31 ruz");
+        break
+    case 2:
+        console.log("fevral")
+        console.log("zima")
+        console.log("29 ruz")
+        break
+    case 3:
+        console.log("mart")
+        console.log("bahor")
+        console.log("31 ruz")
+        break
+    case 4:
+        console.log("aprel")
+        console.log("bahor")
+        console.log("30 ruz")
+        break
+    case 5:
+        console.log("may")
+        console.log("bahor")
+        console.log("31 ruz")
+        break
+    case 6:
+        console.log("iyun")
+        console.log("tobiston")
+        console.log("30 ruz")
+        break
+    case 7:
+        console.log("iyul")
+        console.log("tobiston")
+        console.log("31 ruz")
+        break
+    case 8:
+        console.log("avgust")
+        console.log("tobiston");
+        console.log("30 ruz");
+        break
+    case 9:
+        console.log("sentyabr")
+        console.log("tiramoh")
+        console.log("31 ruz")
+        break
+    case 10:
+        console.log("oktyabr")
+        console.log("tiramoh")
+        console.log("30 ruz")
+        break
+    case 11:
+        console.log("noyabr")
+        console.log("tiramoh")
+        console.log("31 ruz")
+        break
+    case 12:
+        console.log("dekabr")
+        console.log("zima")
+        console.log("30 ruz")
+        break
+    default:
+        fasli_sol = "in xel fasli sol nest !"
+}
+console.log(fasli_sol);
