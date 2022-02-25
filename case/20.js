a8f52feef8f4b56129fbfeeb590492ccdb74d4ca
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-25 16:05:50 +0500

    reshil case20

---
+++ case/20.js
@@ -0,0 +1,57 @@
+let d = parseInt(prompt());
+switch (d) {
+    case 1:
+        console.log("vodoley");
+        console.log("20.1–18.2");
+        break;
+
+    case 2:
+        console.log("ribi");
+        console.log("19.2–20.3");
+        break;
+    case 3:
+        console.log("oven");
+        console.log("21.3–19.4");
+        break;
+    case 4:
+        console.log("telets");
+        console.log("20.4–20.5");
+        break;
+    case 5:
+        c5onsole.log("bliznitsi");
+        console.log("21.5–21.6");
+        break;
+    case 6:
+        console.log("rak");
+        console.log(" 21.5–21.6");
+        break;
+    case 7:
+        console.log("lev");
+        console.log("23.7–22.8");
+        break;
+    case 8:
+        console.log("deva");
+        console.log("23.8–22.9");
+        break;
+    case 9:
+        console.log(" vesi");
+        console.log("23.9–22.10");
+        break;
+    case 10:
+        console.log("skorpion");
+        console.log("23.10–22.11");
+        break;
+    case 11:
+        console.log("strelets");
+        console.log("23.11–21.12");
+        break;
+    case 12:
+        console.log("kozerog");
+        console.log("22.12–19.1");
+        break;
+    default:
+        console.log("in xel sol nest")
+        break;
+
+}
+
