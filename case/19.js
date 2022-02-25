a7b1b280107eeff95a4c98593c06b35733d752c7
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-02-25 15:50:02 +0500

    reshil case19

---
+++ case/19.js
@@ -0,0 +1,61 @@
+let r = parseInt(prompt("r="));
+switch (r) {
+    case 1:
+        console.log("zeleniny");
+        break;
+    case 2:
+        console.log("krasniy");
+        break;
+    case 3:
+        console.log("jeltiy");
+        break;
+    case 4:
+        console.log("beliy");
+        break;
+    case 5:
+        console.log("cherniy");
+        break;
+    default:
+        r = "n xel sol nest"
+}
+let z = parseInt(prompt("z="));
+switch (z) {
+    case 1:
+        console.log("krisi");
+        break;
+    case 2:
+        console.log("karova");
+        break;
+    case 3:
+        console.log("tigra");
+        break;
+    case 5:
+        console.log("zaytsa");
+        break;
+    case 6:
+        console.log("drakona");
+        break;
+    case 7:
+        console.log("zmei");
+        break;
+    case 8:
+        console.log("loshad");
+        break;
+    case 9:
+        console.log("ovtsi");
+        break;
+    case 10:
+        console.log("obezyani");
+        break;
+    case 11:
+        console.log("kuritsi");
+        break;
+    case 12:
+        console.log("sobaki");
+        break;
+    case 13:
+        console.log("svinyi");
+        break;
+    default:
+        z = "in xel sol nest"
+}
