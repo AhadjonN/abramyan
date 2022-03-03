ffe03498c676908d5010eafcaede092fc6bd399c
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-03-03 15:35:50 +0500

    reshil for7

--- for/6.js
+++ for/6.js
@@ -1,4 +1,7 @@
 let a = parseInt(prompt("a="));
 let b = parseInt(prompt("b="));
-for (i = a; i < b; i += 0.2)
-    console.log(a + i + b);
+let y = 0;
+for (i = a; i <= b; i++) {
+    y = y + i
+}
+console.log(y);
