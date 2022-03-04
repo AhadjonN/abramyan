b1e56fac593688530cd49da2fdab308c077111b0
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-03-04 17:10:38 +0500

    reshi for

---
+++ for/24.js
@@ -0,0 +1,11 @@
+let x = parseInt(prompt("x="));
+let n = parseInt(prompt("n="));
+p = x
+s = x
+k = 0
+for (i = 1; i <= n; i++) {
+    k += 2
+    p *= (-1) * x * x / (k * (k + 1));
+    s += p
+}
+console.log(s);
