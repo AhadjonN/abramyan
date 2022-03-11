f73fe3110e40f5c1044596376f0d70f6d7ee6d3b
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-03-11 15:42:08 +0500

    reshil while6

---
+++ while/6.js
@@ -0,0 +1,13 @@
+let n = parseInt(prompt("n="));
+let r = 1;
+if (n % 2 == 0) {
+    console.log(2)
+} else if (n % 2 !== 0) {
+    console.log(1)
+}
+let f = 1;
+while (n >= r) {
+    f *= -n
+    n -= 2
+    console.log(f)
+}
