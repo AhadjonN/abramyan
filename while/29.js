2e97968717c4adc4ddec255a6e2606fbe248c4f8
Nosirov Ahadjon <a.nosirov2005@gmail.com>
2022-03-16 16:37:31 +0500

    reshil while29

---
+++ while/29.js
@@ -0,0 +1,14 @@
+let N = parseInt(prompt());
+let F1 = 1;
+let F2 = 2;
+let F = 2;
+let o = 2;
+while (Math.abs(F - F1) >= N) {
+    F = (F1 + 2 * F2) / 3;
+    F1 = F2;
+    F2 = F;
+    ++o;
+}
+console.log(o);
+console.log(F2);
+console.log(F);
